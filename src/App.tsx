725d6261ca7a3b5361b9220984d50902f1020fb7
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-04-25 18:47:05 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/main_page/header/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Chat from './pages/Chat';
+import HomePage from "./components/main_page/middle/HomePage";
+import NewsHomePage from "./components/main_page/news_fragment/NewsHomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+                <HomePage/>
+                <NewsHomePage/>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
